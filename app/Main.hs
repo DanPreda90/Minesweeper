cf5810c496678d88a106e97254a9c01606c41fd1
Daniel Preda <predad@tcd.ie>
2025-01-02 21:27:29 +0000

    implemented model

--- app/Main.hs
+++ app/Main.hs
@@ -4,29 +4,11 @@ import Lib
 
 import Data.IORef
 import qualified Data.Map.Strict as Map
+import System.Random
 import Control.Monad.IO.Class
+import Control.Monad.Trans.State.Strict
+
 import qualified Graphics.UI.Threepenny as UI
-
-data Status = Empty |
-              Mine  |
-              Clear deriving Show
-
-data Cell = Cell {status::Status,marked::Bool} deriving Show
-
-type Grid = Map.Map (Int,Int) Cell
-
-positions :: Int -> Int -> [((Int,Int),Cell)]
-positions _ 0 = []
-positions size n = (rowPositions size (size-n)) `mappend` (positions size (n-1))
-
-rowPositions :: Int -> Int -> [((Int,Int),Cell)]
-rowPositions n x = take n $ iterate (\((x,y),c) -> ((x,y+1),c)) ((x,0),cell Empty)
-
-grid :: Int -> Grid
-grid size = Map.fromList (positions size size)
-
-cell :: Status -> Cell
-cell s = Cell {status=s,marked=False}
 
 main :: IO ()
 main = do
@@ -36,32 +18,61 @@ setup window = do
   let size = 20
   let cellSize = 20
   let step = 20
+  generator <- initStdGen
+
+  let t = grid size
+  let (g,(m,st)) = runState (initGameState t size generator) (0,Playing)
+  gamestate <- liftIO $ newIORef (m,st)
+  gridstate <- liftIO $ newIORef g
+  
+  currentPosition <- liftIO $ newIORef (0,0)
+  markToggle <- liftIO $ newIORef Clear
+
+  clearBtn <- UI.button UI.#+ [UI.string "Reset "]
+  markCheck <- UI.input UI.# UI.set UI.type_ "checkbox"
+
   canv <- UI.canvas
     UI.# UI.set UI.height 700
     UI.# UI.set UI.width 700
     UI.# UI.set UI.style [("border","solid black 1px"),("background","#eee")]
 
-  g <- liftIO $ newIORef (grid size)
-  currentPosition <- liftIO $ newIORef (0,0)
-
   UI.on UI.mousemove canv $ \xy ->
     do liftIO $ writeIORef currentPosition xy
 
-  UI.on UI.click canv $ \_ ->
-    do pos <- liftIO $ readIORef currentPosition
-       gr <- liftIO $ readIORef g
-       let (x,y) = findCellPos pos (size*cellSize)
-       let grNew = updateGrid gr (x,y)
-       liftIO $ writeIORef g grNew
-       drawGrid canv step (Map.assocs grNew)
-       return ()
+  {-UI.on UI.click clearBtn  $ \_ ->
+    do
+      gen <- newStdGen 
+      let cleared = grid size
+      let newGs = initGameState cleared size gen
+      liftIO $ writeIORef gamestate newGs 
+      drawGrid canv step (Map.assocs (evalState newGs (0,Playing)))-}
 
+  UI.on UI.click canv $ \_ ->
+    do 
+      mark <- liftIO $ readIORef markToggle
+      liftIO $ print mark
+      pos <- liftIO $ readIORef currentPosition
+      gs <- liftIO $ readIORef gamestate
+      gr <- liftIO $ readIORef gridstate
+      let (x,y) = findCellPos pos (fromIntegral $ size*cellSize)
+      let (grNew,gsNew) = runState (updateGameState gr (x,y) mark) gs
+      liftIO $ print gsNew
+      liftIO $ print (x,y)
+      liftIO $ writeIORef gamestate gsNew
+      liftIO $ writeIORef gridstate grNew 
+      drawGrid canv step (Map.assocs grNew)
+      return ()
+
+  UI.on UI.click markCheck $ \_ ->
+    do
+      liftIO $ modifyIORef' markToggle toggleMarkDisplay
+      
   do
-    gr <- liftIO $ readIORef g
+    gr <- liftIO $ readIORef gridstate
     drawGrid canv step (Map.assocs gr)
     return ()
 
-  UI.getBody window UI.#+ [UI.element canv]
+  UI.getBody window UI.#+ [UI.element canv,UI.element clearBtn] UI.#+ [UI.element markCheck, UI.string "Toggle to Mark Tiles"]
   return ()
 
 drawGrid :: UI.Element -> Double -> [((Int,Int),Cell)] -> UI.UI ()
@@ -71,21 +82,14 @@ drawGrid e step (((x,y),c):cs) = do
                                    drawGrid e step cs
 
 drawCell :: UI.Element -> (Double,Double) -> Double -> Cell -> UI.UI ()
+
+drawCell e (x,y) size Cell{mines=(-1)} = do 
+                                      e UI.# UI.set' UI.fillStyle (UI.htmlColor "red")
+                                      e UI.# UI.fillRect (x,y) size size
 drawCell e (x,y) size c = do
                             drawCellStatus e (status c)
                             e UI.# UI.fillRect (x,y) size size
 
 drawCellStatus e Empty = do e UI.# UI.set' UI.fillStyle (UI.htmlColor "#c9c9c9")
-drawCellStatus e Mine = do e UI.# UI.set' UI.fillStyle (UI.htmlColor "red")
+drawCellStatus e Marked = do e UI.# UI.set' UI.fillStyle (UI.htmlColor "green")
 drawCellStatus e Clear = do e UI.# UI.set' UI.fillStyle (UI.htmlColor "black")
-
-updateGrid :: Grid -> (Int,Int) -> Grid
-updateGrid grid (x,y) = Map.adjustWithKey (\_ c -> c{status=Clear}) (x,y) grid
-
-calcDrawPos :: (Int,Int) -> Double -> (Double,Double)
-calcDrawPos (x,y) step = (dx,dy) where dx = (fromIntegral x) * step
-                                       dy = (fromIntegral y) * step
-
-findCellPos :: (Double,Double) -> Double -> (Int,Int)
-findCellPos (x,y) size = (floor $ 20 * (x / size) ,floor $ 20 * (y / size))
-
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,163 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+import qualified Data.Map.Strict as Map
+import Control.Monad.Trans.State.Strict
+import System.Random
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Status = Empty |
+              Marked|
+              Clear deriving Show
+
+data GameStatus = Won |
+                  Failed |
+                  Playing deriving Show
+
+type Grid = Map.Map (Int,Int) Cell
+data Cell = Cell {status::Status,mines::Int} deriving Show
+type GameInfo = (Int,GameStatus)
+type GameState = State GameInfo Grid 
+
+toggleMarkDisplay :: Status -> Status
+toggleMarkDisplay Marked = Clear
+toggleMarkDisplay _ = Marked
+
+initGameState :: RandomGen g => Grid -> Int -> g -> GameState
+initGameState grid size gen = 
+  do
+    let (newGen,mineGrid) = Map.mapAccum addMine gen grid
+    let final = setMineProximities mineGrid (0,0) size
+    let numMines = foldr (\a b -> if (mines a == (-1)) then b+1 else b) 0 mineGrid
+    --setStdGen $ fst mineGrid
+    put (numMines,Playing)
+    return (final)
+
+calcDrawPos :: (Int,Int) -> Double -> (Double,Double)
+calcDrawPos (x,y) step = (dx,dy) where dx = (fromIntegral x) * step
+                                       dy = (fromIntegral y) * step
+
+findCellPos :: (Double,Double) -> Double -> (Int,Int)
+findCellPos (x,y) size = (floor $ 20 * (x / size) ,floor $ 20 * (y / size))
+
+positions :: Int -> Int -> [((Int,Int),Cell)] 
+positions _ 0 = []
+positions size n = (rowPositions size (size-n)) `mappend` (positions size (n-1))
+
+rowPositions :: Int -> Int -> [((Int,Int),Cell)]
+rowPositions n x = take n $ iterate (\((x,y),c) -> ((x,y+1),c)) ((x,0),cell Empty 0)
+
+grid :: Int -> Grid
+grid size = Map.fromList (positions size size)
+
+cell :: Status -> Int -> Cell
+cell s m = Cell {status=s,mines=m}
+
+addMine :: RandomGen g => g -> Cell -> (g,Cell)
+addMine gen c = if (val <= 0.9) then (newGen,cell Empty 0) else (newGen,cell Empty (-1)) where (val,newGen) = uniformR (0::Float,1::Float) gen
+
+setMineProximities :: Grid -> (Int,Int) -> Int -> Grid
+setMineProximities grid (x,y) size 
+  | (x == size) = setMineProximities grid (0,y+1) size
+  | (y == size) = grid
+  | otherwise = setMineProximities (setMineProximity grid (x,y)) (x+1,y) size
+
+setMineProximity grid (x,y) = 
+  case (Map.lookup (x,y) grid) of
+    Just Cell{mines = -1} -> grid
+    Just _ -> let numMines = foldr getMines 0 (getNeighbouringCells grid (x,y))
+                  in Map.adjust (\c -> c{mines=numMines}) (x,y) grid
+    Nothing -> grid
+
+getMines :: Maybe Cell -> Int -> Int
+getMines (Just c) a = if (mines c == (-1)) then a+1 else a
+getMines Nothing a = a
+
+getNeighbouringCells :: Grid -> (Int,Int) -> [Maybe Cell]
+getNeighbouringCells grid (x,y) =   
+  let c1 = Map.lookup (x+1,y) grid
+      c2 = Map.lookup (x-1,y) grid
+      c3 = Map.lookup (x,y+1) grid
+      c4 = Map.lookup (x,y-1) grid
+      c5 = Map.lookup (x+1,y+1) grid
+      c6 = Map.lookup (x+1,y-1) grid
+      c7 = Map.lookup (x-1,y+1) grid
+      c8 = Map.lookup (x-1,y-1) grid
+      in [c1,c2,c3,c4,c5,c6,c7,c8] --[(x+1,y),(x-1,y),(x,y+1),(x,y-1),(x+1,y+1),(x+1,y-1),(x-1,y+1),(x-1,y-1)]
+
+updateGameState :: Grid -> (Int,Int) -> Status -> GameState
+updateGameState gr pos s = evalMove pos s (Map.lookup pos gr) gr
+
+evalMove :: (Int,Int) -> Status -> Maybe Cell -> Grid -> GameState
+evalMove pos Marked (Just c) g = 
+  do
+    gm <- toggleMark pos Marked c g
+    (m,_) <- get
+    if m == 0 then 
+      do 
+        put (0,Won) 
+        return gm 
+    else return gm
+
+evalMove pos Clear (Just c) g = 
+  do
+    case (c) of
+      Cell{status=Marked} -> return g
+      Cell{mines=(-1)} ->
+        do
+          put (0,Failed)
+          return g
+      Cell{status=Empty} -> clearFromPos pos g (Just c)
+      _ -> return g
+        
+evalMove _ _ (Nothing) g = return g
+    
+clearFromPos :: (Int,Int) -> Grid -> Maybe Cell -> GameState
+clearFromPos _ g Nothing = return g
+clearFromPos pos g (Just Cell{status=Clear}) = return g
+clearFromPos (x,y) g (Just c) = 
+  do
+    if ((mines c) == 0) then
+      do
+        a <- clearCell (x,y) g
+        b <- clearFromPos (x+1,y) a (Map.lookup (x+1,y) a)
+        cg <- clearFromPos (x,y+1) b (Map.lookup (x,y+1) b)
+        d <- clearFromPos (x-1,y) cg (Map.lookup (x-1,y) cg)
+        res <- clearFromPos (x-1,y) d (Map.lookup (x,y-1) d)
+        return res
+    else return g
+    
+
+clearCell :: (Int,Int) -> Grid -> GameState
+clearCell pos g = 
+  do
+    let gn = Map.adjust (\c -> c{status=Clear}) pos g
+    return gn
+
+
+toggleMark :: (Int,Int) -> Status -> Cell -> Grid -> GameState
+toggleMark pos s c g = 
+  do
+    (ms,st) <- get
+    case (c) of
+      Cell{status=Clear} -> return g
+      Cell{status=Marked} ->
+        do
+          if (mines c == (-1)) then 
+            do
+              put (ms+1,st)
+              let gn = Map.adjust (\c -> c{status=Empty}) pos g
+              return gn
+          else
+            do
+              let gn = Map.adjust (\c -> c{status=Empty}) pos g
+              return gn
+      Cell{status=Empty} ->
+        do
+          if (mines c == (-1)) then 
+            do
+              put (ms-1,st)
+              let gn = Map.adjust (\c -> c{status=Marked}) pos g
+              return gn
+          else
+            do
+              let gn = Map.adjust (\c -> c{status=Marked}) pos g
+              return gn
+                              
